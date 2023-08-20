6c4e12d57b62a39d631aa151d0a822cb9a3170c4
Marmik kumar <110611754+Marmikkumar@users.noreply.github.com>
2023-08-20 20:50:21 +0530

    Add files via upload

--- backend/form.js
+++ backend/form.js
@@ -1,1 +1,23 @@
-Added new folder
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 5000; // You can change this to any port you prefer
+
+app.use(bodyParser.json());
+
+
+app.get('/api' , (req,res)=>{
+    res.send("form server");
+})
+
+app.post("/api/form", (req, res) => {
+  const form = req.body;
+  console.log('Received form data:', form);
+  // Here you can process the formData and send a response back
+
+  res.json({ message: 'Form data received successfully!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
