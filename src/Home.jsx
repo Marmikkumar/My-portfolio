4eea2c50f4ac63393e5d1622f8adb2c7074c2742
Marmik kumar <110611754+Marmikkumar@users.noreply.github.com>
2023-08-21 00:00:35 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import myimage from "./myimage.png;
 import "./styles.css";
 
 export default function Home() {
@@ -21,7 +22,7 @@ export default function Home() {
         </div>
         <div className="fig">
           <img
-            src="https://scontent.fpat3-2.fna.fbcdn.net/v/t39.30808-6/295062744_748933913013473_3326987055742169383_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=w_17tLcD08IAX9ne27o&_nc_ht=scontent.fpat3-2.fna&oh=00_AfCVni5OAoPIKu2zL7Yk1PIUXZ56rRgcz5tqJnZ_NfwfeQ&oe=64E1AA53"
+            src={myimage}
             alt="photo"
           ></img>
         </div>
